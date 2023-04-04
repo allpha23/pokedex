4495effcaa91005f86d88d693430a056c3d0b1ab
allpha23 <allpha23@gmail.com>
2023-04-03 23:08:48 -0300

    feat: add home page e seu css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,42 @@
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Card from '@/components/Card';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const maxPokemons = 251;
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+}
+
+export default function Home({ pokemons }) {
   return (
-    <h1>Pokedex</h1>
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>Pokedex</h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="pokeball"
+        />
+      </div>
+      <div className={styles.pokemons_container}>
+        {pokemons.map((pokemon) => (
+          <Card key={ pokemon.id } pokemon={ pokemon } />
+        ))}
+      </div>
+    </>
   );
 }
